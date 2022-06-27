b5fd5e815a41166159f395d493574d9fd39b6a1f
Eliáš Sidon <sidonismo@gmail.com>
2022-06-27 07:27:53 +0200

    funkcni json

--- js/sd.js
+++ js/sd.js
@@ -1,4 +1,5 @@
 const sd = document.createElement('script');
+let it = 0;
 sd.setAttribute('type', 'application/ld+json');
 var requestOptions = {
     method: 'GET',
@@ -12,6 +13,12 @@ var requestOptions = {
     sd.textContent = textData;
     document.head.appendChild(sd);
   }); */
+  sd.textContent = `{ 
+    "@context": "https://schema.org",
+    "@type": "ItemList",
+    "url": "https://sidonismo.github.io/responsive_a11/index2.html",
+    "numberOfItems": 5842,
+    "itemListElement": [` 
 
   fetch("https://antikvariat.textrix.cz/api/home", requestOptions)
   .then(response => response.json())
@@ -22,6 +29,7 @@ var requestOptions = {
       const oid = element.oid;
       const itid = element.itid;
       let title = '';
+      let title2 = '';
       let autor = '';
       let rok = 0;
       let stran = '';
@@ -30,10 +38,14 @@ var requestOptions = {
       let field = {}
       const url = element.url;
 
+
       for (let i = 0; i < fieldsLength; i++) {
         field = element.fields[i];
-        if (field.dc === 'title') {
+        if (field.label === 'Název') {
           title = field.val;
+        }
+        if (field.label === 'Další název') {
+          title2 = field.val;
         }
         if (field.label === 'Autor') {
           autor = field.val;
@@ -47,225 +59,42 @@ var requestOptions = {
         if (field.label === 'Kategorie') {
           kat.push(field.val);
         }
-        if (field.label === "Cena") {
+        if (field.tag === "price") {
           cena = field.val;
         }
       }
-
-    sd.textContent = 
-        `{"@context": "https://schema.org",
-        "@type": "ItemList",
-        "url": "https://sidonismo.github.io/responsive_a11/index2.html",
-        "numberOfItems": 5842,
-        "itemListElement": [
-            {
+      sd.textContent += `{
                 "@type": "Book",
-                "position": 1,
-                "image": ["https://antikvariat.textrix.cz/img/439.jpg", "https://antikvariat.textrix.cz/assets/cache/439-tn.png"],
-                "url": "https://sidonismo.github.io/kniha/komensky-jan-amos-veskerych-spisu-jana-amosa-komenskeho-svazek-xvii-1912",
-                "name": "Veškerých Spisů Jana Amosa Komenského. Svazek XVII",
+                "position": ${it+1},
+                "image": ["https://antikvariat.textrix.cz/assets/cache/${oid}-tn.png", "https://antikvariat.textrix.cz/img/${oid}.jpg"],
+                "url": "https://antikvariat11.cz${url}",
+                "name": "${title}",
+                "alternativeHeadline" : "${title2}",
                 "offers": {
                     "@type": "Offer",
-                    "price": "100",
+                    "price": ${cena},
                     "priceCurrency": "CZK",
                     "itemCondition": "https://schema.org/UsedCondition",
                     "availability": "https://schema.org/InStock"
                 },
-                "genre": "spisy",
+                "genre": "${kat.toString()}",
                 "author": {
                     "@type": "Person",
-                    "givenName": "Jan Amos",
-                    "familyName": "Komenský"
+                    "givenName": "${autor}"
                 },
-                "numberOfPages": "534"
-            },
-            {
-                "@type": "Book",
-                "position": 2,
-                "image": ["https://antikvariat.textrix.cz/img/439.jpg", "https://antikvariat.textrix.cz/assets/cache/439-tn.png"],
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Tibetská kniha mrtvých : Bardo thödol (Vysvobození v bardu skrze naslouchání)",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "náboženství",
-                "numberOfPages": "165"
-            },
-            {
-                "@type": "Book",
-                "position": 3,
-                "image": ["https://antikvariat.textrix.cz/assets/cache/446-tn.png", "https://antikvariat.textrix.cz/img/446.jpg"],
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Conan : hodina draka",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "Fantasy",
-                "author": {
-                    "@type": "Person",
-                    "givenName": "Robert Ervin",
-                    "familyName": "Howard"
-                },
-                "numberOfPages": "229"
-            },
-            {
-                "@type": "Book",
-                "position": 4,
-                "image": "https://antikvariat.textrix.cz/assets/cache/443-tn.png",
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Živá díla minulosti ; sv. 109",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "náboženství",
-                "author": {
-                    "@type": "Person",
-                    "givenName": "Douglas",
-                    "familyName": "Adams"
-                },
-                "numberOfPages": "598"
-            },
-            {
-                "@type": "Book",
-                "position": 5,
-                "image": "https://antikvariat.textrix.cz/assets/cache/443-tn.png",
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Živá díla minulosti ; sv. 109",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "náboženství",
-                "author": {
-                    "@type": "Person",
-                    "givenName": "Douglas",
-                    "familyName": "Adams"
-                },
-                "numberOfPages": "598"
-            },
-            {
-                "@type": "Book",
-                "position": 6,
-                "image": "https://antikvariat.textrix.cz/assets/cache/443-tn.png",
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Živá díla minulosti ; sv. 109",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "náboženství",
-                "author": {
-                    "@type": "Person",
-                    "givenName": "Douglas",
-                    "familyName": "Adams"
-                },
-                "numberOfPages": "598"
-            },
-            {
-                "@type": "Book",
-                "position": 7,
-                "image": "https://antikvariat.textrix.cz/assets/cache/443-tn.png",
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Živá díla minulosti ; sv. 109",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "náboženství",
-                "author": {
-                    "@type": "Person",
-                    "givenName": "Douglas",
-                    "familyName": "Adams"
-                },
-                "numberOfPages": "598"
-            },
-            {
-                "@type": "Book",
-                "position": 8,
-                "image": "https://antikvariat.textrix.cz/assets/cache/443-tn.png",
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Živá díla minulosti ; sv. 109",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "náboženství",
-                "author": {
-                    "@type": "Person",
-                    "givenName": "Douglas",
-                    "familyName": "Adams"
-                },
-                "numberOfPages": "598"
-            },
-            {
-                "@type": "Book",
-                "position": 9,
-                "image": "https://antikvariat.textrix.cz/assets/cache/443-tn.png",
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Živá díla minulosti ; sv. 109",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "náboženství",
-                "author": {
-                    "@type": "Person",
-                    "givenName": "Douglas",
-                    "familyName": "Adams"
-                },
-                "numberOfPages": "598"
-            },
-            {
-                "@type": "Book",
-                "position": 10,
-                "image": "https://antikvariat.textrix.cz/assets/cache/443-tn.png",
-                "url": "http://127.0.0.1:5501/kniha/tibetska-kniha-mrtvych-bardo-thodol-vysvobozeni-v-bardu-skrze-naslouchani-1991-80-207-0334-9-vaz",
-                "name": "Živá díla minulosti ; sv. 109",
-                "offers": {
-                    "@type": "Offer",
-                    "price": "200",
-                    "priceCurrency": "CZK",
-                    "itemCondition": "https://schema.org/UsedCondition",
-                    "availability": "https://schema.org/InStock"
-                },
-                "genre": "náboženství",
-                "author": {
-                    "@type": "Person",
-                    "givenName": "Douglas",
-                    "familyName": "Adams"
-                },
-                "numberOfPages": "598"
-            }
-        ]
+                "numberOfPages": ${stran.match(/^(\d+)/)[0]}
+            }`
+console.log("it = " ,it);
+    if (it === 9){
+        sd.textContent += `]
     }`;
-    document.head.appendChild(sd);
+    } else {
+        sd.textContent += `,`;
+    }
+            
+    it++;
     });
 })
 .catch(error => console.log('error', error));
+
+    document.head.appendChild(sd);
